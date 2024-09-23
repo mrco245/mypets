0b9affe331efc931cccebda8252d86ea2d690a9b
Madison <cheertatter@icloud.com>
2024-09-22 20:34:16 -0400

    remoing secrets

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,23 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { DatePipe } from '@angular/common';
 
+const firebaseConfig = {
+  projectId: '',
+  appId: '',
+  storageBucket: '',
+  apiKey: '',
+  authDomain: '',
+  messagingSenderId: '',
+  measurementId: '',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [ AuthService, provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
-    { provide: FIREBASE_OPTIONS, useValue: {"projectId":"mypets-40d8d","appId":"1:892940366473:web:ca6768aedaf494cdf58c58","storageBucket":"mypets-40d8d.appspot.com","apiKey":"AIzaSyBnsjYsz8KN2RvtlYmVGUwof_I6Ni2XUBU","authDomain":"mypets-40d8d.firebaseapp.com","messagingSenderId":"892940366473","measurementId":"G-0XG969HLNF"}}, provideAnimationsAsync(), provideNativeDateAdapter(),]
+  providers: [
+    AuthService,
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: FIREBASE_OPTIONS, useValue: {firebaseConfig} },
+    provideAnimationsAsync(),
+    provideNativeDateAdapter(),
+  ],
 };
