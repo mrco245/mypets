2d186308b66af96e93b349abb2358d81fb0731cc
Madison <cheertatter@icloud.com>
2024-09-23 22:52:24 -0400

    updates

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,4 @@
 export const config = {
     MONGODB_URI : 'mongodb+srv://admin:admin@cluster0.nt77z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    JWT_SECRET: 'bdf4057be34cdb7ce64ee8ab7f2a26c2ebf5a0a0560cc1d977bd49c1e0080099'
+    JWT_SECRET: ''
 }
