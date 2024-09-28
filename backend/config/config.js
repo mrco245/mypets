6ad89b2d6c0b9b7fabab666ec7eac25d70b82171
Madison <cheertatter@icloud.com>
2024-09-28 17:48:41 -0400

    updates

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,8 @@
+import crypto from 'crypto'
+
+const secret = crypto.randomBytes(32).toString('hex'); 
+
 export const config = {
     MONGODB_URI : 'mongodb+srv://admin:admin@cluster0.nt77z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    JWT_SECRET: ''
+    JWT_SECRET: secret
 }
